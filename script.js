275d9538ae821007f53e8b00c10cf2efd1645f86
Jesse Willig <jessewillig@outlook.com>
2021-01-10 21:02:32 -0800

    added function variables

--- script.js
+++ script.js
@@ -13,16 +13,37 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// When button clicked, prompt for password criteria
+// Generate password function
+function generatePassword() {
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var symbols = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "<", ">", "_", "-"];
 
-// User select password criteria
+// Ask password length w/ while loop including alert
+while (passwordLength > 8 && passwordLength < 128) {
+  prompt("Please select password length greater than 8 and less than 128 characters.")
+}
 
-// Prompt for passwrod length, 8-128 characters
+// Ask if user wants to use lowercase letters
+  // Add or exclude lowercase letters w/ if/else
+  // Acceptable character push
 
-// Prompt character types, Upper/lower case letters, numbers, symbols
+// Ask if user wants to use uppercase letters
+  // Add or exclude uppercase letters w/ if/else
+  // Acceptable character push
 
-// Input valid when at least one character type selected
+// Ask if user wants to use numbers letters
+  // Add or exclude numbers letters w/ if/else
+  // Acceptable character push
 
-// When all prompts answered, password is generated that meets users selected criteria
+// Ask if user wants to use symbols letters
+  // Add or exclude symbols letters w/ if/else
+  // Acceptable character push
 
-// Once password generated, is displayed as alert or written to the page
+// Edge case to prompt if user does not select 1 character type
+
+// For loop to random select ok characters
+
+// Array concatenate. Create a string from array
+}
