12a13d8094214f64872e199e120414f9d52da972
Jesse Willig <jessewillig@outlook.com>
2021-01-11 23:00:11 -0800

    added if/else statements w/ concat

--- script.js
+++ script.js
@@ -19,27 +19,64 @@ function generatePassword() {
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var symbols = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "<", ">", "_", "-"];
+  var passwordLength = 0;
+  var userCriteria = [];
+  var randomPassword = [];
 
 // Ask password length w/ while loop including alert
-while (passwordLength > 8 && passwordLength < 128) {
-  prompt("Please select password length greater than 8 and less than 128 characters.")
+while (passwordLength > 8 || passwordLength < 128) {
+  passwordLength = prompt("Please select password length greater than 8 and less than 128 characters.");
+  console.log(passwordLength);
 }
 
 // Ask if user wants to use lowercase letters
+var askLower = confirm("Do you want lower case letters?");
+
   // Add or exclude lowercase letters w/ if/else
-  // Acceptable character push
+  if (askLower === true) {
+      // Acceptable character push
+    okayChar=okayChar.concat(lowerCase)
+    console.log("Lower case OK: " + true);
+  } else {
+    console.log("Lower case OK: " + false);
+  }
 
 // Ask if user wants to use uppercase letters
+var askUpper = confirm("Do you want upper case letters?");
+
   // Add or exclude uppercase letters w/ if/else
-  // Acceptable character push
+  if (askUpper === true) {
+      // Acceptable character push
+      okayChar=okayChar.concat(upperCase)
+    console.log("Upper case OK: " + true);
+  } else {
+    console.log("Upper case OK: " + false);
+  }
 
-// Ask if user wants to use numbers letters
-  // Add or exclude numbers letters w/ if/else
-  // Acceptable character push
+// Ask if user wants to use numbers
+var askNumber = confirm("Do you want numbers?");
 
-// Ask if user wants to use symbols letters
-  // Add or exclude symbols letters w/ if/else
-  // Acceptable character push
+  // Add or exclude numbers w/ if/else
+  if (askNumber === true) {
+      // Acceptable character push
+    okayChar=okayChar.concat(numbers)
+    console.log("Numbers OK: " + true);
+  } else {
+    console.log("Numbers OK: " + false);
+  }
+
+// Ask if user wants to use symbols
+var askSymbols = confirm("Do you want symbols?");
+
+  // Add or exclude symbols w/ if/else
+  if (askLower === true) {
+     // Acceptable character push
+    okayChar=okayChar.concat(symbols)
+    console.log("Symbols OK: " + true);
+  } else {
+    console.log("Symbols OK: " + false);
+  }
+ 
 
 // Edge case to prompt if user does not select 1 character type
 
